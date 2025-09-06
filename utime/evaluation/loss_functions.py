1e3525860341956b540d7e781cd6404503f7958f
Ali Saberi <ali.saberi96@gmail.com>
2025-09-06 17:34:18 +0200

    Add weighted categorical crossentropy loss function and wrapper class

--- utime/evaluation/loss_functions.py
+++ utime/evaluation/loss_functions.py
@@ -50,3 +50,57 @@ class SparseDiceLoss(tf.keras.losses.Loss):
 
     def call(self, y_true, y_pred):
         return sparse_dice_loss(y_true, y_pred, smooth=self.smooth)
+
+
+def weighted_categorical_crossentropy(y_true, y_pred, class_weights=None):
+    """
+    Implements weighted categorical crossentropy loss.
+    Particularly useful for imbalanced classification tasks.
+
+    Args:
+        y_true: Ground truth labels, should be one-hot encoded or sparse
+        y_pred: Model predictions (softmax probabilities)
+        class_weights: Optional dictionary or list of class weights.
+                      If None, all classes are weighted equally.
+
+    Returns:
+        Weighted categorical crossentropy loss value
+    """
+    y_true, shape, n_classes = _get_shapes_and_one_hot(y_true, y_pred)
+    
+    # Handle class weights
+    if class_weights is None:
+        class_weights = tf.ones([n_classes])
+    elif isinstance(class_weights, dict):
+        class_weights = tf.convert_to_tensor([class_weights.get(i, 1.0) for i in range(n_classes)])
+    else:
+        class_weights = tf.convert_to_tensor(class_weights)
+    
+    # Ensure weights are float32
+    class_weights = tf.cast(class_weights, tf.float32)
+    
+    # Compute weighted cross entropy
+    y_pred = tf.clip_by_value(y_pred, tf.keras.backend.epsilon(), 1.0 - tf.keras.backend.epsilon())
+    cross_entropy = -y_true * tf.math.log(y_pred)
+    weighted_cross_entropy = cross_entropy * class_weights
+    
+    return tf.reduce_mean(tf.reduce_sum(weighted_cross_entropy, axis=-1))
+
+
+class WeightedCategoricalCrossentropy(tf.keras.losses.Loss):
+    """tf.keras.losses.Loss wrapper for weighted_categorical_crossentropy"""
+    
+    def __init__(self,
+                 class_weights=None,
+                 reduction=tf.keras.losses.Reduction.AUTO,
+                 name='weighted_categorical_crossentropy'):
+        super().__init__(name=name, reduction=reduction)
+        self.class_weights = class_weights
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({'class_weights': self.class_weights})
+        return config
+
+    def call(self, y_true, y_pred):
+        return weighted_categorical_crossentropy(y_true, y_pred, self.class_weights)
